3b89c3a478375275ff902408927ecb52490c760e
Taishi Ueda <taishi.ueda@roms.inc>
2021-10-19 14:25:35 +0900

    Update ver. #

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Distutils import build_ext
 
 setup(
     name='ikfastpy',
-    version='0.0.1',
+    version='0.0.2',
     package_dir={'ikfastpy': ''},
     packages=['ikfastpy'],
     ext_modules=[
