cdc429a883329ca7586a124d9d1999bebe5ae298
MoriKen254 <masamorobo@gmail.com>
2021-10-19 13:28:00 +0900

    Modify setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,21 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
-setup(ext_modules=[Extension("ikfastpy", 
-                            ["ikfastpy.pyx", 
-                             "ikfast_wrapper.cpp"], language="c++", libraries=['lapack'])],
-      cmdclass = {'build_ext': build_ext})
+setup(
+    name='ikfastpy',
+    version='0.0.1',
+    package_dir={'ikfastpy': ''},
+    packages=['ikfastpy'],
+    ext_modules=[
+        Extension(
+            ".",
+            ["ikfastpy.pyx", "ikfast_wrapper.cpp"],
+            language="c++",
+            libraries=['lapack']
+            )],
+    cmdclass = {'build_ext': build_ext}
+    )
+# setup(ext_modules=[Extension("ikfastpy", 
+#                            ["ikfastpy.pyx", 
+#                             "ikfast_wrapper.cpp"], language="c++", libraries=['lapack'])],
+#      cmdclass = {'build_ext': build_ext})
