0cbc82364b6879e8b367c3a1b7f3792ea9548c38
Roms in Kojiya Ubuntu <git-roms@nosuchanaddress.com>
2021-12-23 17:44:10 +0900

    model was modified

--- setup.py
+++ setup.py
@@ -4,15 +4,20 @@ from Cython.Distutils import build_ext
 
 setup(
     name='ikfastpy',
-    version='0.0.1',
+    version='0.0.3',
     package_dir={'ikfastpy': ''},
     packages=['ikfastpy'],
     ext_modules=[
         Extension(
-            ".",
+            "ikfastpy",
             ["ikfastpy.pyx", "ikfast_wrapper.cpp"],
             language="c++",
             libraries=['lapack']
             )],
     cmdclass = {'build_ext': build_ext}
     )
+
+# setup(ext_modules=[Extension("ikfastpy", 
+#                             ["ikfastpy.pyx", 
+#                              "ikfast_wrapper.cpp"], language="c++", libraries=['lapack'])],
+#       cmdclass = {'build_ext': build_ext})
